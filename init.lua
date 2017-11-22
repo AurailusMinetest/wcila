c1f17c1349c0cdc6b7ccfabbcf166a091e8c757b
Aurailus <100Toby1@gmail.com>
2017-11-22 15:22:35 -0800

    1.2: Recentered preview image, preview image will be 3d if type of node is a block, ignore airlike and liquid drawtypes, ignore items with group 'not_wcila_visible'

--- init.lua
+++ init.lua
@@ -2,6 +2,71 @@ wcila = {}
 wcila.huds = {}
 local show_image = true
 
+--Check WCILA Visibility
+local function wcila_visible(node)
+   if node == "air" then return false end
+   local def = minetest.registered_items[node]
+   if def.drawtype == "airlike" or def.drawtype == "liquid" or def.drawtype == "flowingliquid" then return false end
+   if def.groups.not_wcila_visible and defs.groups.not_wcila_visible ~= 0 then return false end
+   return true
+end
+
+local function getFirstTex(tex) 
+   -- string.sub(tex, 1, 
+   tex = string.sub(tex, 1, (string.find(tex, '^', 1, true) or string.len(tex)+1)-1)
+   return tex
+end
+
+--Create WCILA Hud
+local function create_wcila_hud(player)
+   local elems = {}
+   elems.bg = player:hud_add({
+      hud_elem_type = "image",
+      position = {x = 0.5, y = 0},
+      scale = {x = 0, y = 0},
+      name = "WAILA Background",
+      text = "wcila_bg.png",
+      alignment = 0,
+      offset = {x = 0, y = 34},
+      direction = 0
+   })
+   elems.tooltip = player:hud_add({
+      hud_elem_type = "text",
+      position = {x = 0.5, y = 0},
+      scale = {x = 100, y = 100},
+      number = 0xFFFFFF,
+      alignment = 0,
+      offset = {x = 20, y = 22},
+      direction = 0,
+      name = "WAILA Display Nmae",
+      text = display_name,
+   })
+   elems.technical = player:hud_add({
+      hud_elem_type = "text",
+      position = {x = 0.5, y = 0},
+      scale = {x = 100, y = 100},
+      number = 0xCCCCCC,
+      alignment = 0,
+      offset = {x = 20, y = 42},
+      direction = 0,
+      name = "WAILA Technical Name",
+      text = name,
+   })
+   elems.img = player:hud_add({
+      hud_elem_type = "image",
+      position = {x = 0.5, y = 0},
+      scale = {x = s, y = s},
+      name = "WAILA Block Image",
+      text = "",
+      alignment = 0,
+      offset = {x = -(100 / 2 * 3), y = 32.5},
+      direction = 0
+   })
+
+   wcila.huds[player:get_player_name()] = elems
+end
+
+--Detect and show block
 function wcila.update(player)
    local loc = player:getpos()
    local hor = math.rad(math.deg(player:get_look_horizontal()) + 90)
@@ -14,79 +79,63 @@ function wcila.update(player)
       zmod = i * math.sin(ver) * math.sin(hor) --y
       ymod = i * math.cos(ver) --z
       name = minetest.get_node({x = loc.x + xmod, y = loc.y + 1.65 + ymod, z = loc.z + zmod}).name
-      if name ~= "air" then break end
+      if wcila_visible(name) then break end
    end
 
-   if name == "air" then name = "" end
-   local display_name = name
+   if not wcila_visible(name) then name = "" end
+   local display_name = ""
    if name ~= "" and minetest.registered_items[name].description ~= "" then display_name = minetest.registered_items[name].description end
    local s = 0
+   local techoff = 42
+   local iscale = 1
    if name ~= "" then s = 1 end
+   if display_name == "" then techoff = 32 end
    local image = ""
-   if show_image then
-      if minetest.registered_items[name] and minetest.registered_items[name].tiles then
-         if minetest.registered_items[name].tiles[1] and type(minetest.registered_items[name].tiles[1]) ~= "table" then
+   if minetest.registered_items[name] and minetest.registered_items[name].tiles then
+      if minetest.registered_items[name].tiles[1] and type(minetest.registered_items[name].tiles[1]) ~= "table" then
+         local dt = minetest.registered_items[name].drawtype
+         if dt == "normal" or dt == "allfaces" or dt == "allfaces_optional" 
+         or dt == "glasslike" or dt =="glasslike_framed" or dt == "glasslike_framed_optional" 
+         or dt == "liquid" or dt == "flowingliquid" then
+            local tiles = minetest.registered_items[name].tiles 
+            
+            local top = tiles[1]
+            if (type(top) == "table") then top = getFirstTex(top.name) else
+               if top then
+                  top = getFirstTex(top)
+               end
+            end
+            local left = tiles[3]
+            if (type(left) == "table") then left = getFirstTex(left.name) else
+               if left then
+                  left = getFirstTex(left)
+               else left = top end
+            end
+            local right = tiles[5]
+            if (type(right) == "table") then right = getFirstTex(right.name) else
+               if right then
+                  right = getFirstTex(right)
+               else right = left end
+            end
+            
+            image = "[inventorycube{" .. top .. "{" .. left .. "{" .. right
+            iscale = 0.3
+         else
             image = minetest.registered_items[name].tiles[1]
          end
       end
    end
 
    if not wcila.huds[player:get_player_name()] then
-      local elems = {}
-      elems.bg = player:hud_add({
-         hud_elem_type = "image",
-         position = {x = 0.5, y = 0},
-         scale = {x = s, y = s},
-         name = "WAILA Background",
-         text = "wcila_bg.png",
-         alignment = 0,
-         offset = {x = 0, y = 34},
-         direction = 0
-      })
-      elems.tooltip = player:hud_add({
-         hud_elem_type = "text",
-         position = {x = 0.5, y = 0},
-         scale = {x = 100, y = 100},
-         number = 0xFFFFFF,
-         alignment = 0,
-         offset = {x = 20, y = 22},
-         direction = 0,
-         name = "WAILA Display Nmae",
-         text = display_name,
-      })
-      elems.technical = player:hud_add({
-         hud_elem_type = "text",
-         position = {x = 0.5, y = 0},
-         scale = {x = 100, y = 100},
-         number = 0xCCCCCC,
-         alignment = 0,
-         offset = {x = 20, y = 42},
-         direction = 0,
-         name = "WAILA Technical Name",
-         text = name,
-      })
-      if show_image then
-         elems.img = player:hud_add({
-            hud_elem_type = "image",
-            position = {x = 0.5, y = 0},
-            scale = {x = s, y = s},
-            name = "WAILA Block Picture",
-            text = "",
-            alignment = 0,
-            offset = {x = -(100 / 2 * 3), y = 33},
-            direction = 0
-         })
-      end
-      wcila.huds[player:get_player_name()] = elems
+      create_wcila_hud(player)
    else
       local elems = wcila.huds[player:get_player_name()]
       player:hud_change(elems.bg, "scale", {x = s * 3, y = s * 3})
       player:hud_change(elems.tooltip, "text", display_name)
       player:hud_change(elems.technical, "text", name)
-      if show_image then 
-         player:hud_change(elems.img, "text", tostring(image)) 
-         player:hud_change(elems.img, "scale", {x = s * 2.5, y = s * 2.5})
-      end
+      player:hud_change(elems.technical, "offset", {x = 20, y = techoff})
+      player:hud_change(elems.img, "text", tostring(image)) 
+      player:hud_change(elems.img, "scale", {x = s * 2.5 * iscale, y = s * 2.5 * iscale})
    end
 end
 
